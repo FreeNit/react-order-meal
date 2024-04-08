2e237852fe0bde7678609a148c9df1bebff86f9d
Dimon <bolilui.dmytro@gmail.com>
2024-04-08 18:09:21 +0300

    Added reusable Button component

---
+++ src/UI/Button.jsx
@@ -0,0 +1,12 @@
+import { Children } from "react";
+
+export default function Button({ children, textOnly, className, ...props }) {
+  let cssClasses = textOnly ? "text-button" : "button";
+  cssClasses += " " + className;
+
+  return (
+    <button {...props} className={cssClasses}>
+      {children}
+    </button>
+  );
+}
